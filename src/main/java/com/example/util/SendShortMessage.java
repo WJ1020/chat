7a627918af358cf77e1fb775863de63e9b88866b
WangShiXiang <wangshixiang1020@gmail.com>
2017-04-09 23:04:02 +0800

    增加了短信发送的工具类

---
+++ src/main/java/com/example/util/SendShortMessage.java
@@ -0,0 +1,60 @@
+package com.example.util;
+
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by WangShiXiang on 2017/4/9.
+ * 短信发送类
+ */
+public class SendShortMessage {
+
+    public static void send(String text,String[] phone){
+        try {
+            URL url=new URL("http://utf8.sms.webchinese.cn");
+            HttpURLConnection httpURLConnection=(HttpURLConnection) url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setUseCaches(false);
+            httpURLConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+            httpURLConnection.connect();
+
+            DataOutputStream outputStream=new DataOutputStream(httpURLConnection.getOutputStream());
+            StringBuffer stringBuffer=new StringBuffer();
+            for (String str:phone){
+                stringBuffer.append(str).append(",");
+            }
+            String str=stringBuffer.toString();
+            str=str.substring(0,str.length()-1);
+            str=URLEncoder.encode(str,"utf-8");
+            //http://utf8.sms.webchinese.cn/?Uid=本站用户名&Key=接口安全秘钥&smsMob=手机号码&smsText=验证码:8888
+            String content="Uid="+ URLEncoder.encode("17862901361abc","utf-8")+"&Key="+URLEncoder.encode("","utf-8")+"&smsMob="+str+"&smsText="+URLEncoder.encode(text,"utf-8");
+            outputStream.writeBytes(content);
+            outputStream.flush();
+            outputStream.close();
+
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String line;
+            while ((line=bufferedReader.readLine())!=null){
+                System.out.println(line);
+            }
+            bufferedReader.close();
+            httpURLConnection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+//    public static void main(String[] args){
+//        String[] phone=new String[]{"17862901361","17862901383"};
+//        String text="验证码1020";
+//        send(text,phone);
+//    }
+}
